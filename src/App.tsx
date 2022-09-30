8f2f3751f05df63086ff899408aba34169a473ea
Artyom Avanesov <artsyom.avanesov@gmail.com>
2022-09-30 05:30:59 +0300

    add lingopractices-models

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 import SignWithGoogle from '@components/SignWithGoogle/SignWithGoogle';
+import { ITopic } from 'lingopractices-models';
+
+
+const exampleTopic: ITopic = {
+  id: 1,
+  name: '2',
+  questions: [],
+};
+
+console.warn(exampleTopic);
 
 const App = () => (
   <div>
