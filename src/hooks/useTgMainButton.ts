f55952e1a02e281446b09ca92911711f7d2c6df6
yasinski yan <yasinskijan161@gmail.com>
2022-10-07 18:41:13 +0300

    refactor(#51): move types on the top

--- src/hooks/useTgMainButton.ts
+++ src/hooks/useTgMainButton.ts
@@ -2,6 +2,12 @@ import { useCallback, useEffect } from 'react';
 
 import { MainButtonParams } from '../telegram/types';
 
+type MainButtonType = {
+  setBackButtonOnClick: (fn: () => void) => void;
+  setMainButtonParams: (obj: MainButtonParams) => void;
+  setLoadingMainButton: (state: boolean) => void;
+};
+
 const useTgMainButton = (
   isVisibleMainButton: boolean,
   isEnabledMainButton: boolean,
@@ -12,9 +18,15 @@ const useTgMainButton = (
     return fn;
   }, []);
 
-  const setLoadingMainButton = useCallback((state: boolean) => window.Telegram.WebApp.MainButton.showProgress(state), []);
+  const setLoadingMainButton = useCallback(
+    (state: boolean) => window.Telegram.WebApp.MainButton.showProgress(state),
+    [],
+  );
 
-  const setMainButtonParams = useCallback((obj: MainButtonParams) => window.Telegram.WebApp.MainButton.setParams(obj), []);
+  const setMainButtonParams = useCallback(
+    (obj: MainButtonParams) => window.Telegram.WebApp.MainButton.setParams(obj),
+    [],
+  );
 
   useEffect(
     () => () => {
@@ -39,9 +51,3 @@ const useTgMainButton = (
 };
 
 export default useTgMainButton;
-
-type MainButtonType = {
-  setBackButtonOnClick: (fn: () => void) => void;
-  setMainButtonParams: (obj: MainButtonParams) => void;
-  setLoadingMainButton: (state: boolean) => void;
-};
