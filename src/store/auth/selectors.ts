cdf03aee37fe50c71efcc43bf994f9f42bcdcf89
yasinski yan <yasinskijan161@gmail.com>
2022-11-11 12:42:47 +0300

    fix(#84): fix init auth

--- src/store/auth/selectors.ts
+++ src/store/auth/selectors.ts
@@ -1,3 +1,4 @@
 import { RootState } from '@store/store';
 
 export const securityTokenSelector = (state: RootState) => state.auth.securityToken;
+export const isAuthenticatedSelector = (state: RootState) => state.auth.isAuthenticated;
