b5e89c2bf950b9113e667f1c27b378cc30ca2bbe
yasinski yan <yasinskijan161@gmail.com>
2022-12-25 22:03:05 +0300

    fix(#134): fix zindex, add listener of failed actions

--- src/store/alerts/reducer.ts
+++ src/store/alerts/reducer.ts
@@ -1,10 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit';
 
 import { CreateAlert } from './features/create-alert/create-alert';
+import { CreateAlertFailure } from './features/create-alert/create-alert-failure';
 import { CreateAlertSuccess } from './features/create-alert/create-alert-success';
 import { GetAlerts } from './features/get-alerts/get-alerts';
 import { GetAlertsSuccess } from './features/get-alerts/get-alerts-success';
 import { UpdateAlert } from './features/update-alert/update-alert';
+import { UpdateAlertFailure } from './features/update-alert/update-alert-failure';
 import { UpdateAlertSuccess } from './features/update-alert/update-alert-success';
 import { IAlertState } from './types';
 
@@ -23,8 +25,10 @@ const reducer = createReducer(initialState, (builder) =>
     .addCase(GetAlertsSuccess.action, GetAlertsSuccess.reducer)
     .addCase(CreateAlert.action, CreateAlert.reducer)
     .addCase(CreateAlertSuccess.action, CreateAlertSuccess.reducer)
+    .addCase(CreateAlertFailure.action, CreateAlertFailure.reducer)
     .addCase(UpdateAlert.action, UpdateAlert.reducer)
-    .addCase(UpdateAlertSuccess.action, UpdateAlertSuccess.reducer),
+    .addCase(UpdateAlertSuccess.action, UpdateAlertSuccess.reducer)
+    .addCase(UpdateAlertFailure.action, UpdateAlertFailure.reducer),
 );
 
 export default reducer;
